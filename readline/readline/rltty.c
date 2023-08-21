e98a051459fc382767a1473742ba126a687d777c
Vincent Rivière <vincent.riviere@freesbee.fr>
2023-08-22 00:04:59 +0200

    Add workaround for stairs effect on signals

--- readline/readline/rltty.c
+++ readline/readline/rltty.c
@@ -529,7 +529,7 @@ prepare_terminal_settings (int meta_flag, TIOTYPE oldtio, TIOTYPE *tiop)
     tiop->c_iflag &= ~(ISTRIP | INPCK);
 
   /* Make sure we differentiate between CR and NL on input. */
-  tiop->c_iflag &= ~(ICRNL | INLCR);
+//  tiop->c_iflag &= ~(ICRNL | INLCR);
 
 #if !defined (HANDLE_SIGNALS)
   tiop->c_lflag &= ~ISIG;
